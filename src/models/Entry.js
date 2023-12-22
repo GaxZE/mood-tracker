085b417ff11eef6b412513223f97eca300ce050b
Gary Hawes <gary.hawes@gmail.com>
2023-12-22 11:04:50 +0000

    Starting from scratch

--- src/models/Entry.js
+++
@@ -1,8 +0,0 @@
-const db = require("../db");
-const { Schema } = require("mongoose");
-
-const EntrySchema = new Schema({
-  _id: { type: Schema.ObjectId, auto: true, alias: "id" },
-  date: { type: Date, default: Date.now },
-  comment: String
-});
