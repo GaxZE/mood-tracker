62e5bca618bf6f0db73e28179fa4bec1a1672224
Gary Hawes <gary.hawes@gmail.com>
2023-12-22 15:27:44 +0000

    Basic entry

--- mood/__main__.py
+++ mood/__main__.py
@@ -1,10 +1,13 @@
 import click
+from datetime import date
 
 @click.command()
-@click.option('--rating', prompt=True, type=int)
-@click.option('--comment', prompt=True, type=str)
-def main(rating, comment):
-    click.echo("Rating for today has been stored.")
+@click.option('--rating', required=True, prompt=True, type=int)
+@click.option('--comment', required=False, prompt=True, type=str)
+@click.option('--date', prompt=True, required=True, type=click.DateTime(formats=["%Y-%m-%d"]),
+              default=str(date.today()))
+def main(rating, comment, date):
+    click.echo(f"Rating of {rating} for {date} has been stored.")
 
 
 if __name__ == '__main__':
